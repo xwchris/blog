d6fd83d7c7104664fefb338f2941d76be2b36d23
xwchris <xwchris.zhang@gmail.com>
2021-07-10 11:47:09 +0800

    chore

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(overfronted.net www.overfronted.net)
+domains=(overfronted.net)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="xwchris.zhang@gmail.com" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
