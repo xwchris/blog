5c0b1445bca56b2d9e2133dfca14040d6f534e78
xwchris <xwchris.zhang@gmail.com>
2021-07-10 11:58:36 +0800

    chore: 修改部署脚本

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(overfronted.net)
+domains=(overfronted.net www.overfronted.net)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="xwchris.zhang@gmail.com" # Adding a valid address is strongly recommended
